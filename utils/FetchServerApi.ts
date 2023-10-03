5effd25b549c177891222583a2453f2a350a4b82
yeison-gonzalez <yeison.gonzalez01@hotmail.com>
2023-10-02 23:16:44 -0500

    [ADD] add fetch server api

--- utils/FetchServerApi.ts
+++ utils/FetchServerApi.ts
@@ -7,14 +7,14 @@ import { IRequest, ITicker } from "@/models/Ticker"
  * @returns Promise<ITicker[]>
  */
 export const fetchGetAllTickers = async (): Promise<ITicker[]> => {
-  return fetch(`${process.env.BASE_FETCH_URL}/api/tickers`, { next: { revalidate: 60 } })
+  return fetch(`${process.env.API_URL}/tickers/?limit=10`, { next: { revalidate: 60 } })
     .then(res => res.json())
     .then(res => res.data)
 }
 
 export const fetchGetTickerDetail = async ({ coin }: IRequest): Promise<ITicker[]> => {
   if (coin) {
-    return fetch(`${process.env.BASE_FETCH_URL}/api/tickers/search?coin=${coin}`)
+    return fetch(`${process.env.API_URL}/ticker/?id=${coin}`)
       .then(res => res.json())
       .then(res => res.data || res)
   }
@@ -22,7 +22,7 @@ export const fetchGetTickerDetail = async ({ coin }: IRequest): Promise<ITicker[
 }
 
 export const fetchGetMarketByCoin = async ({ id }: IRequestMarket): Promise<IMarket[]> => {
-  return fetch(`${process.env.BASE_FETCH_URL}/api/markets?coin=${id}`)
+  return fetch(`${process.env.API_URL}/coin/markets/?id=${id}`)
     .then(res => res.json())
     .then(res => res.slice(0, 5))
 }
